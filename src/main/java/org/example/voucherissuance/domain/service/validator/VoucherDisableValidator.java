e6e49fc156de83d217271230931e98f0be743f7a
duddnddnfl <duddnddnfl@naver.com>
2024-12-26 12:12:53 +0900

    리팩토링 - 검증 로직 추가 수정

--- src/main/java/org/example/voucherissuance/domain/service/validator/VoucherDisableValidator.java
+++ src/main/java/org/example/voucherissuance/domain/service/validator/VoucherDisableValidator.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class VoucherDisableValidator {
     public void validate(VoucherEntity voucherEntity, RequestContext requestContext) {
+        상품권_사용_불가_처리_권한이_있는지_확인(voucherEntity, requestContext);
+    }
+    // 메서드 이름을 한글로 쓰는 것을 추천 (Validator 안에서만)
+    private static void 상품권_사용_불가_처리_권한이_있는지_확인(VoucherEntity voucherEntity, RequestContext requestContext) {
         if(voucherEntity.publishHistory().getRequesterType() != requestContext.requesterType()
                 || !voucherEntity.publishHistory().getRequesterId().equals(requestContext.requesterId())){
             throw new IllegalArgumentException("사용 불가 처리 권한이 없는 상품권입니다.");
--- src/main/java/org/example/voucherissuance/domain/service/validator/VoucherPublishValidator.java
+++ src/main/java/org/example/voucherissuance/domain/service/validator/VoucherPublishValidator.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class VoucherPublishValidator {
-
     public void validate(ContractEntity contractEntity) {
+        상품권_발행을_위한_계약_유효기간이_확인되었는지_확인(contractEntity);
+    }
+
+    // 메서드 이름을 한글로 쓰는 것을 추천 (Validator 안에서만)
+    private static void 상품권_발행을_위한_계약_유효기간이_확인되었는지_확인(ContractEntity contractEntity) {
         if (contractEntity.isExpired()) {
             throw new IllegalStateException("유효기간이 지난 계약입니다.");
         }
